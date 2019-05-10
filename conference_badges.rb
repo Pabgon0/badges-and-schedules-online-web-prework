00e47877f39392719b9aa493b328be2df8535374
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-10 01:40:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  namelist = []
+  names.each do |name|
+  namelist << badge_maker(name)
+  end
+  namelist
+end
+
+def assign_rooms(list_of_speakers)
+  rooms_list = []
+  room_number = 1
+  list_of_speakers.each do |name|
+    rooms_list << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    room_number += 1
+  end
+  rooms_list
+end
+
+def printer
+  batch_badge_creator
+  batch_badge_creator.each do |badge|
+    puts badge
+  end
+  assign_rooms
+  assign_rooms.each do |room|
+    puts room
+  end
+end
